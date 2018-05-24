44dfc5331e39fcab336c7274a0927893057b807d
Markus Gut <markusgut@users.noreply.github.com>
2018-05-24 10:51:30 +0200

    Prettified code.
    - content.js was simplified, restructured, and commented
    - consent-settings.js is now one closure

--- consent-settings.js
+++ consent-settings.js
@@ -1,23 +1,38 @@
 /**
  * ContentJS: JavaScript library for managing user's consent
- * @file Exposes javascript functions to alter consent data. Use this code on your website's privacy settings page, after consent.js.
+ * @file Exposes functions needed to manipulate consent data. Use this code on your website's privacy settings page.
  * @author Markus Gut
  * @copyright gut.digital 2018
  * @license Apache-2.0
  */
 
-ConsentJS.saveConsent = function() {
-    localStorage.setItem('ConsentJS', JSON.stringify(window.ConsentJS.consent));
-};
-ConsentJS.setOptout = function(tracker) {
-    console.log('yay!')
-    this.consent[tracker] = this.consent[tracker] || {};
-    this.consent[tracker].optout = Date.now();
-    ConsentJS.saveConsent();
-};
-ConsentJS.removeOptout = function(tracker) {
-    console.log('yay!')
-    this.consent[tracker] = this.consent[tracker] || {};
-    this.consent[tracker].optout = 0;
-    ConsentJS.saveConsent();
-};
+(function(obj) {
+    var setters = {
+            saveConsent: function() {
+                localStorage.setItem('ConsentJS', JSON.stringify(window.ConsentJS.consent));
+            },
+
+            setOptout: function(tracker) {
+                this.consent[tracker] = this.consent[tracker] || {};
+                this.consent[tracker].optout = Date.now();
+                ConsentJS.saveConsent();
+            },
+
+            removeOptout: function(tracker) {
+                this.consent[tracker] = this.consent[tracker] || {};
+                this.consent[tracker].optout = 0;
+                ConsentJS.saveConsent();
+            }
+        },
+        extend = function(obj, src) {
+            for (var key in src) {
+                if (src.hasOwnProperty(key)) obj[key] = src[key];
+            }
+            return obj;
+        };
+
+    // Extend main object
+    ConsentJS = extend(obj, setters);
+
+})(ConsentJS);
+
--- consent.js
+++ consent.js
@@ -7,28 +7,37 @@
  */
 
 var ConsentJS = {
-    'init': function(trackers) {
-        /* read whatever data we find */
-        this.consent = JSON.parse(localStorage.getItem('ConsentJS')) || {};
+    'consent': // read previously stored data
+        JSON.parse(localStorage.getItem('ConsentJS')) || {},
 
-        /* make sure all expected properties do exist */
-        this.consent = this.consent || {};
-        for (var index = 0; index < trackers.length; ++index) {
-            this.consent[trackers[index]] = this.consent[trackers[index]] || {};
+    'init': function(trackers) { // initialize trackers
+        for (var index = 0, length = trackers.length; index < length; ++index) {
+            this.consent[trackers[index]] = this.consent[trackers[index]] || {}; // add tracker if there was no saved data about it
+            // add tracker's properties
             this.consent[trackers[index]].optout = this.consent[trackers[index]].optout || 0;
-        }        
-    },
-    'getDNT': function() {
-        var DNT = (navigator.doNotTrack || window.doNotTrack || navigator.msDoNotTrack || '');
-        return (DNT.indexOf('1') === 0) || (DNT.indexOf('yes') === 0);
-    },
-    'getConsent': function(tracker) {
-        if (!this.consent[tracker]) { // no consent data for tracker
-            console.log('ConsentJS: Undefined tracker \''+ tracker + '\'');
-            return false;
-        } else {
-            return !this.getDNT() && (this.consent[tracker].optout === 0);
         }
     },
-    'consent': {}
+
+    'getDNT': function() {
+       // Get DNT header values from different browsers. See:
+        // https://developer.mozilla.org/en-US/docs/Web/API/Navigator/doNotTrack#Browser_compatibility
+        var DNT = (navigator.doNotTrack || window.doNotTrack || navigator.msDoNotTrack || '');
+
+        // DNT is active if header value starts with '1' or 'yes'. See:
+        // https://developer.mozilla.org/en-US/docs/Web/API/Navigator/doNotTrack#Browser_compatibility
+        return (DNT.indexOf('1') === 0) || (DNT.indexOf('yes') === 0);
+    },
+
+    'getConsent': function(tracker) {
+        // do we know this tracker?
+        if (!this.consent[tracker]) {
+            // If we don't know this tracker, maybe you forgot to update your init() statement.
+            console.log('ConsentJS: \'' + tracker + '\'' + ' unknown');
+            // Play safe: don't let an unknown tracker stalk your visitors
+            return false;
+        } else {
+            // no DNT? no optout, either? there you go!
+            return !this.getDNT() && (this.consent[tracker].optout === 0);
+        }
+    }
 };
